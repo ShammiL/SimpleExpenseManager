5bd0acfb25377e8e30fd78eb5845982aa19bd0f0
Shammi <shammi0107@gmail.com>
2019-12-19 19:13:00 +0530

    Update DatabaseHelper.java

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/DatabaseHelper.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
@@ -24,7 +26,7 @@ public class DatabaseHelper extends SQLiteOpenHelper
 
     public DatabaseHelper(@Nullable Context context) {
         super(context,DATABASE_NAME,null,1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
@@ -37,5 +39,61 @@ public class DatabaseHelper extends SQLiteOpenHelper
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("drop table if exists "+TABLE_NAME_1);
         onCreate(db);
+    }
+
+    public boolean insertAccount(String Account_no,String bank, String Account_holder,double initial_balance){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues ctVal = new ContentValues();
+        ctVal.put(col_1_1,Account_no);
+        ctVal.put(col_1_2,bank);
+        ctVal.put(col_1_3,Account_holder);
+        ctVal.put(col_1_4,initial_balance);
+        long result = db.insert(TABLE_NAME_1,null,ctVal);
+        if (result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean insertTransaction(String date,String Account_no, String expense_type,double amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues ctVal = new ContentValues();
+        ctVal.put(col_1_1,date);
+        ctVal.put(col_1_2,Account_no);
+        ctVal.put(col_1_3,expense_type);
+        ctVal.put(col_1_4,amount);
+        long result = db.insert(TABLE_NAME_1,null,ctVal);
+        if (result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllAccountData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME_1,null);
+        return res;
+    }
+
+    public Cursor getAllTransactionData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME_2,null);
+        return res;
+    }
+
+    public boolean updateData(String Account_no,String bank,String Account_holder,double initial_balance) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_1_1,Account_no);
+        contentValues.put(col_1_2,bank);
+        contentValues.put(col_1_3,Account_holder);
+        contentValues.put(col_1_4,initial_balance);
+        db.update(TABLE_NAME_1, contentValues, "Account_no = ?",new String[] { Account_no });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME_1, "ID = ?",new String[] {id});
     }
 }
